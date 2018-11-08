09e747c1f6997389dd320aefe88862e361409330
Samilly Nunes <samillynunes19@gmail.com>
2018-11-08 18:29:44 -0300

    Testando a primeira classe de domínio: Categoria

--- src/main/java/com/samilly/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/samilly/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.samilly.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.samilly.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias") //esse value eh o endpoint do endereco do navegador
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET) //Para pegar/recuperar um dado
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria (2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>(); //List eh uma interface
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
