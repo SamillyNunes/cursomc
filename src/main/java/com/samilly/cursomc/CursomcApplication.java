cf6761626476c628714aaa845649d11287c49a13
Samilly Nunes <samillynunes19@gmail.com>
2018-11-09 21:41:00 -0300

    Operacao de instanciacao: salvando dados automaticamente

--- src/main/java/com/samilly/cursomc/CursomcApplication.java
+++ src/main/java/com/samilly/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.samilly.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.samilly.cursomc.domain.Categoria;
+import com.samilly.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{ //Essa interface implementada permite que seja configurado para que o spring execute alguma ação quando o programa iniciar
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception { //metodo que executa algumas acoes quando o programa inicia
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
